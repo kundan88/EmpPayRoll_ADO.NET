bc731985f33f07984dff384c7f7e3d4c50d58cd5
Kundan <kundan kamble>
2022-09-08 16:35:09 -0800

    Eshtablish Connection

---
+++ Employee_PayRoll/EmployeeRepo.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_PayRoll
+{
+    public class EmployeeRepo
+    {
+        public static string ConnectionString = "Data Source=(localdb)\\MSSQLLOCALDB;Initial Catalog=payroll_service;Integrated Security=True";
+        SqlConnection connection = new SqlConnection(ConnectionString);
+
+        public void EstablishConnection()
+        {
+            if (connection != null && connection.State.Equals(ConnectionState.Closed))
+            {
+                try
+                {
+                    connection.Open();
+                }
+                catch (Exception)
+                {
+                    throw new EmployeeException(EmployeeException.ExceptionType.CONNECTION_FAILED, "Connection Not Found");
+                }
+            }
+        }
+        public void CloseConnection()
+        {
+            if (connection != null && connection.State.Equals(ConnectionState.Open))
+            {
+                try
+                {
+                    connection.Close();
+                }
+                catch (Exception)
+                {
+                    throw new EmployeeException(EmployeeException.ExceptionType.CONNECTION_FAILED, "Connection Not Found");
+                }
+            }
+        }
+    }
+}
+
+
